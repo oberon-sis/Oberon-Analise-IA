e984f549761c83ed799f955fdf76a06a9466a7f8
JhoelDiego2 <jhoel.mita@sptech.school>
2025-11-23 13:38:19 -0300

    feat: retorno esperado para o front end

--- app/services/analise_previsao.py
+++ app/services/analise_previsao.py
@@ -31,12 +31,42 @@ def processar_request_previsao(analise_req: AnaliseRequest):
     
     logger.debug(datas_historico)
     logger.debug(valores)
+    return processar_dados_front_end()
 
-    return {
+def processar_dados_front_end():
+    """
+    resultado esperado enviar um json formtado assim para o front end
+    response = {
+    iaMetricas: {
+        interpretacao: [
+            'Após o pico de Abril, o Downtime estabilizou-se em torno de 55. A projeção (Mock) indica um aumento gradual para 65 até Setembro, sugerindo atenção imediata.',
+            'A análise desta tendência de alertas foi realizada utilizando Regressão Linear sobre o histórico semanal de dados. O Índice de Confiabilidade do modelo é de 85%, indicando alta aderência da projeção à variação real observada.',
+        ],
+        metricasRegressao: { R: '0.92', R2: '0.85', RSE: '5.2%', indiceConfiabilidade: '85%' },
+    },
+    graficoData: {
+        labels_Dia: ['1 Nov', '2 Nov', '3 Nov', '4 Nov', '5 Nov', '6 Nov', '7 Nov'],
+        labels_Semana: ['Sem 44', 'Sem 45', 'Sem 46', 'Sem 47', 'Sem 48', 'Sem 49'],
+        labels_Mês: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
+        dataAtual: [65, 59, 80, 81, 56, 55],
+        dataAnterior: [50, 45, 70, 75, 40, 42],
+    },
+    
+    """
+
+    # return {
+    #     "analise_tipo": "previsao",
+    #     "agrupamento": agrupar_por,
+    #     "previsao": 'previsoes',
+    #     "historico": valores,
+    #     "datas_historico": datas_historico,
+    #     "insight_ia": 'insight_ia'
+    # }
+    return { #teste
         "analise_tipo": "previsao",
-        "agrupamento": agrupar_por,
+        "agrupamento": "agrupar_por",
         "previsao": 'previsoes',
-        "historico": valores,
-        "datas_historico": datas_historico,
+        "historico": "valores",
+        "datas_historico": "datas_historico",
         "insight_ia": 'insight_ia'
     }
