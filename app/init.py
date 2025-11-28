b1c6e6a068ac217c2086717ff17804d647082bf2
JhoelDiego2 <jhoel.mita@sptech.school>
2025-11-28 00:36:06 -0300

    fix: configuração do logger

--- app/init.py
+++ app/init.py
@@ -3,14 +3,12 @@
 from flask import Flask
 import logging 
 
+logger = logging.getLogger(__name__)
+
 def create_app():
     app = Flask(__name__)
-
-    logging.basicConfig(
-        level=logging.INFO, 
-        format='[%(asctime)s] %(levelname)s in %(module)s.%(funcName)s: %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
+    
+    logger.info('Started')
     
     from .routes.routes_analisis import ai_bp
     app.register_blueprint(ai_bp, url_prefix="/ai")
